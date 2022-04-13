c7d9dc71b0815fc76e685f7600889e1cf7eab937
don <misurakalu@gmail.com>
2022-04-13 14:51:38 +1000

    add youtube playlists

--- python_time.py
+++ python_time.py
@@ -4,13 +4,13 @@ import webbrowser
 def six_am_routine():
     print(time.strftime("%H:%M:%S"))
     print("It's TIME!!\nGET AFTER IT!!\n")
-    webbrowser.open('http://net-informations.com', new=())
+    webbrowser.open('https://www.youtube.com/playlist?list=PL_6yYTbdnwfmdOf5SSzzljlhZIahw5NUX', new=())
 
 
 def nighty_night():
     print(time.strftime("%H:%M:%S"))
     print("turn other devices off\n turn on the alarm\nturn off the lights\ntime to sleep\n")
-    webbrowser.open('http://net-informations.com', new=())
+    webbrowser.open('https://www.youtube.com/playlist?list=PL_6yYTbdnwflAZuIhuM_nIgJPXAePYuth', new=())
       
 if __name__ == "__main__":
     while time.strftime("%H:%M:%S") != "13:41:00" or "22:00:00":
