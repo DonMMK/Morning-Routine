7ae7543c2ccd3b2668e7c1086fb2596de2aa33c8
don <misurakalu@gmail.com>
2022-04-13 21:35:38 +1000

    Update python_time.py

--- python_time.py
+++ python_time.py
@@ -8,7 +8,7 @@ def six_am_routine():
     print(time.strftime("%H:%M:%S"))
     print("It's TIME!!\nGET AFTER IT!!\n")
     
-    # Morning GUI
+    # Morning GUI w Positive Self Talk & Affirmations
     root = Tk()
     root.geometry("400x200")
     Label(root,text="Alarm Clock",font=("Helvetica 20 bold"),fg="red").pack(pady=10)
@@ -22,7 +22,7 @@ def six_am_routine():
 
 def nighty_night():
     print(time.strftime("%H:%M:%S"))
-    # gui telling you to stop doing work
+    # gui telling you to stop doing work wind down
     
     # Timer to close other applications
     print("turn other devices off\n turn on the alarm\nturn off the lights\ntime to sleep\n")
@@ -30,7 +30,7 @@ def nighty_night():
     webbrowser.open('https://www.youtube.com/watch?v=wM5CCu2NH2E&list=PL_6yYTbdnwflAZuIhuM_nIgJPXAePYuth', new=())
       
 if __name__ == "__main__":
-    while time.strftime("%H:%M:%S") != "06:00:00" or "22:00:00":
+    while time.strftime("%H:%M:%S") != "18:36:00" or "22:00:00":
       time.sleep(1)
-      if time.strftime("%H:%M:%S") == "06:00:00": six_am_routine()		
+      if time.strftime("%H:%M:%S") == "18:36:00": six_am_routine()		
       if time.strftime("%H:%M:%S") == "22:00:00": nighty_night()
