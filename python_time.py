f5c643ca42d8077e1e7fe6fb1e946edc7b03a363
don <misurakalu@gmail.com>
2022-04-13 13:56:05 +1000

    Update

--- python_time.py
+++ python_time.py
@@ -1,11 +1,19 @@
 import time
+import webbrowser
 
 def six_am_routine():
     print(time.strftime("%H:%M:%S"))
-    print("It's TIME!!")
+    print("It's TIME!!\nGET AFTER IT!!\n")
+    webbrowser.open('http://net-informations.com', new=())
 
+
+def nighty_night():
+    print(time.strftime("%H:%M:%S"))
+    print("turn other devices off\n turn on the alarm\nturn off the lights\ntime to sleep\n")
+    webbrowser.open('http://net-informations.com', new=())
+      
 if __name__ == "__main__":
-    while time.strftime("%H:%M:%S") != "12:39:00":
+    while time.strftime("%H:%M:%S") != "13:41:00" or "22:00:00":
       time.sleep(1)
-    else:
-        six_am_routine()		
+      if time.strftime("%H:%M:%S") == "13:41:00": six_am_routine()		
+      if time.strftime("%H:%M:%S") == "22:00:00": nighty_night()
